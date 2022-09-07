ff5868e7c428a128051ca1bf92d46237f78487aa
gustavo9601 <tavo9601@gmail.com>
2022-09-07 00:25:32 -0500

    init proyect

---
+++ src/main/java/springbootmicroservicesconfigdragon/springbootmicroservicesconfigdragon/SpringBootMicroservicesConfigDragonApplication.java
@@ -0,0 +1,15 @@
+package springbootmicroservicesconfigdragon.springbootmicroservicesconfigdragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringBootMicroservicesConfigDragonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroservicesConfigDragonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springbootmicroservicesconfigdragon/springbootmicroservicesconfigdragon/SpringBootMicroservicesConfigDragonApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootmicroservicesconfigdragon.springbootmicroservicesconfigdragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesConfigDragonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
